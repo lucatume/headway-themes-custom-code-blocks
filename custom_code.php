3273f8c712e44fa123fa8c7cf1433b21b98bfecc
Luca Tumedei <luca@theaveragedev.com>
2014-01-10 15:37:55 +0800

    initial commit

---
+++ custom_code.php
@@ -0,0 +1,14 @@
+<?php
+$imgSrc = null;
+// if it's not a page return
+if (!is_page($post->ID) or !current_theme_supports('post-thumbnails') or !has_post_thumbnail($post->ID)) {
+	$imgSrc = get_header_image();
+}
+else {
+	$imgSrc = wp_get_attachment_url(get_post_thumbnail_id($psot->ID ));
+}
+// create a CSS inline style string to use the post thumbnail as the block-element bg
+
+$cssInlineStyleString = 'style="background-image:url(' .$imgSrc.');background-size:cover;"';
+
+?>
